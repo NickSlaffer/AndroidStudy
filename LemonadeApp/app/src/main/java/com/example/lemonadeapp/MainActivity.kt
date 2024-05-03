146fd5039d3d3bc8c36bd6f1bd25eddce21d9133
NickSlaffer <nickslaffer@gmail.com>
2024-05-04 00:43:16 +0300

    update final project

--- LemonadeApp/app/src/main/java/com/example/lemonadeapp/MainActivity.kt
+++ LemonadeApp/app/src/main/java/com/example/lemonadeapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,15 +16,24 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lemonadeapp.ui.theme.LemonadeAppTheme
 
@@ -44,12 +54,87 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
+@OptIn(ExperimentalMaterial3Api::class)
 fun LemonadeApp(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+
+    var currentStep by  remember { mutableStateOf(1)}
+
+    var squeezeCount by remember { mutableStateOf(0)}
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Lemonade",
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                colors = TopAppBarDefaults.largeTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        }
+    ) { innerPadding ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(MaterialTheme.colorScheme.tertiaryContainer),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            when (currentStep) {
+                1 -> {
+                    TextAndImage(
+                        textLabel = R.string.LemonTreeText,
+                        drawable = R.drawable.lemon_tree,
+                        contentDescription = R.string.LemonTree,
+                        onClick = {
+                            currentStep = 2
+                            squeezeCount = (2..4).random()
+                        }
+                    )
+                }
+                2 -> {
+                    TextAndImage(
+                        textLabel = R.string.LemonText,
+                        drawable = R.drawable.lemon,
+                        contentDescription = R.string.Lemon,
+                        onClick = {
+                            squeezeCount--
+                            if (squeezeCount == 0) {
+                                currentStep = 3
+                            }
+                        }
+                    )
+                }
+
+                3 -> {
+                    TextAndImage(
+                        textLabel = R.string.LemonadeText,
+                        drawable = R.drawable.glass_lemonade,
+                        contentDescription = R.string.Lemonade,
+                        onClick = {
+                            currentStep = 4
+                        }
+                    )
+                }
+                4 -> {
+                    TextAndImage(
+                        textLabel = R.string.EmptyGlassText,
+                        drawable = R.drawable.empty_glass,
+                        contentDescription = R.string.EmptyGlass,
+                        onClick = {
+                            currentStep = 1
+                        }
+                    )
+                }
+            }
+        }
+    }
+
 }
 
 @Composable
